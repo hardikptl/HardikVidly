115c05d86c3b374729ee5c79205ea3bac063fd83
Hardikkumar Patel <hardikpkptl@outlook.com>
2020-01-19 00:22:16 -0500

    added new theme from bootwatch.com , addes .css file under content folder add reference to bundle.config under App_Start.

--- HardikVidly/App_Start/BundleConfig.cs
+++ HardikVidly/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace HardikVidly
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.css", "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
